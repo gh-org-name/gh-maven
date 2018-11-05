f2e16e30c6d6a2b9bc60f33cdbc96786a26d14a6
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-11-05 16:33:19 +0100

    adding new issues

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -6,7 +6,9 @@ package com.jcg.maven;
  */
 public class App 
 {
-    public static void main( String[] args )
+    private static final int MAX_PRICE = 10;
+
+    public static void main(String[] args )
     {
         System.out.println( "Hello World!" );
     }
@@ -16,6 +18,27 @@ public class App
         System.out.println("notCovered");
     }
 
+
+    public void newMethod(){
+        int target = -5;
+        int num = 3;
+
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
+
+
+        String str = "/File|Name.txt";
+
+        String clean = str.replaceAll(".",""); // Noncompliant; probably meant to remove only dot chars, but returns an empty string
+        String clean2 = str.replaceAll("|","_"); // Noncompliant; yields _/_F_i_l_e_|_N_a_m_e_._t_x_t_
+
+
+    }
+
+    public void setPrice(int price) {
+        assert price >= 0 && price <= MAX_PRICE;
+        // Set the price
+    }
 
 
     public class Car{
