071f1f3497b06099161b878c12f1eba866d00847
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-12-04 10:31:13 +0100

    adding issues

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -6,8 +6,54 @@ package com.jcg.maven;
  */
 public class App 
 {
-    public static void main( String[] args )
+    private static final int MAX_PRICE = 10;
+
+    public static void main(String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
+
+
+    public void newMethodGitchecks(){
+        int target = -5;
+        int num = 3;
+
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
+
+
+        String str = "/File|Name.txt";
+
+        String clean = str.replaceAll(".",""); // Noncompliant; probably meant to remove only dot chars, but returns an empty string
+        String clean2 = str.replaceAll("|","_"); // Noncompliant; yields _/_F_i_l_e_|_N_a_m_e_._t_x_t_
+
+
+    }
+
+    public void setPriceGitChecks(int price) {
+        assert price >= 0 && price <= MAX_PRICE;
+        // Set the price
+    }
+
+
+    public class CarGitChecks{
+
+        public DriveTrain drive;
+
+        public void tearDown(){
+            System.out.println("Tear Down");
+        }
+
+        public void drive() {System.out.println("Drive");}  // Noncompliant; duplicates field name
+    }
+
+
+    public void aMethod(){
+        int i=0;
+        while (i<10){
+            i++;
+        }
+    }
+
 }
