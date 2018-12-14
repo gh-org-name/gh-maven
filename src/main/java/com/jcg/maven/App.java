7434588ce91d9a8799e2735f5fcea6aae015facc
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-12-14 11:43:44 +0100

    adding issues

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -25,6 +25,9 @@ public class App
         target =- num;  // Noncompliant; target = -3. Is that really what's meant?
         target =+ num; // Noncompliant; target = 3
 
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
+
 
         String str = "/File|Name.txt";
 
