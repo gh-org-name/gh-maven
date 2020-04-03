37d42d95d3b7fc759dbb4f866ef4204158694b99
Aurélie Boiteux <aurelie.boiteux@sonarsource.com>
2020-04-03 07:54:45 +0000

    App.java edited online with Bitbucket

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -9,8 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        //TODO todo
-        //TODO hey
     }
 
     public void newMethodGitchecks(){
